298893d462e8fc9433a7d07ca2c9df7f1112f785
doduyhao2002@gmail.com <doduyhao2002@gmail.com>
2023-06-17 15:55:14 +0700

    add libraries, introActivity, logo app

---
+++ app/src/main/java/com/example/book_reading_app/Activity/IntroActivity.java
@@ -0,0 +1,16 @@
+package com.example.book_reading_app.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.book_reading_app.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_app);
+    }
+}
--- app/src/main/java/com/example/book_reading_app/Activity/MainActivity.java
+++ app/src/main/java/com/example/book_reading_app/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.book_reading_app;
+package com.example.book_reading_app.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.book_reading_app.R;
 
 public class MainActivity extends AppCompatActivity {
 
